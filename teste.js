3e0a4f5b992ca0b2e47c597b1059ff3b21d02ced
Arthur Meireles <arthurmeireles070@gmail.com>
2020-05-10 17:17:35 -0300

    mostrando o primeiro grafico

---
+++ teste.js
@@ -0,0 +1,26 @@
+var chart = null;
+$(document).ready(function(){
+    chart = Highcharts.chart('container', {
+        data: {
+          table: 'datatable'
+        },
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Data extracted from a HTML table in the page'
+        },
+        yAxis: {
+          allowDecimals: false,
+          title: {
+            text: 'Units'
+          }
+        },
+        tooltip: {
+          formatter: function () {
+            return '<b>' + this.series.name + '</b><br/>' +
+              this.point.y + ' ' + this.point.name.toLowerCase();
+          }
+        }
+      });
+})
